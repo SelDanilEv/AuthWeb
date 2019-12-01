e40fdecd89b18fcea5f8bc00a6e8ae5c8433b6bd
Danil <sdanilev@gmail.com>
2019-12-01 15:50:07 +0300

    First try

---
+++ TryAuth2/Pages/Account.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TryAuth2.Pages
+{
+    public class AccountModel : PageModel
+    {
+        public string Username;
+
+        public void OnGet()
+        {
+            Username = HttpContext.Session.GetString("username");
+        }
+    }
+}
